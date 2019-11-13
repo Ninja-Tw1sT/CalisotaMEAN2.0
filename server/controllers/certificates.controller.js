ca8e6386346d72de6cd575903b9672f78c1d7481
M Manzer <lazer_bud2011@hotmail.com>
2019-11-13 16:19:24 +0500

    Update routes and Add controllers for certificates, schools and employments

---
+++ server/controllers/certificates.controller.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+const _ = require('lodash');
+
+const Certificate = mongoose.model('Certificate', {
+  userId: {type: String},
+  issuer: {type: String},
+  certification: {type: String},
+  certNumber: {type: String},
+});
+
+let addCertificate = (req, res, next) => {
+  var certificate = new Certificate();
+  certificate.userId = req._id;
+  certificate.issuer = req.body.issuer;
+  certificate.certification = req.body.certification;
+  certificate.certNumber = req.body.certNumber;
+
+  certificate.save((err, doc) => {
+    if (!err) {
+      res.status(200).send(doc);
+    } else {
+      if (err.code == 11000)
+          res.status(422).send(['Duplicate entry found.']);
+      else
+          return res.status(400).send(err);
+    }
+  })
+};
+
+let getCertificates = (req, res, next) => {
+  Certificate.find({ userId: req._id },
+    (err, certs) => {
+        if (!certs)
+            return res.status(404).json({ status: false, message: 'No certificates found.' });
+        else
+            return res.status(200).json({ status: true, certificates : certs });
+    }
+  );
+};
+
+module.exports = {
+  addCertificate,
+  getCertificates
+};
--- server/routes/index.router.js
+++ server/routes/index.router.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const ctrlUser = require('../controllers/user.controller');
+const ctrlCertificates = require('../controllers/certificates.controller');
+const ctrlEmployments = require('../controllers/employments.controller');
+const ctrlSchools = require('../controllers/schools.controller');
 
 const jwtHelper = require('../config/jwtHelper');
 
 router.post('/register', ctrlUser.register);
 router.post('/authenticate', ctrlUser.authenticate);
 router.get('/userProfile',jwtHelper.verifyJwtToken, ctrlUser.userProfile);
+
+// certificates
+router.post('/certificates', ctrlCertificates.addCertificate);
+router.get('/certificates', ctrlCertificates.getCertificates);
+
+// employments
+router.post('/employments', ctrlEmployments.addEmployment);
+router.get('/employments', ctrlEmployments.getEmployments);
+
+// schools
+router.post('/schools', ctrlSchools.addSchools);
+router.get('/schools', ctrlSchools.getSchools);
 
 module.exports = router;
 
