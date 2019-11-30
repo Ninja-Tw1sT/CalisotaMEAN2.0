271f86f8189ce1b4b3c40eaa21711378ea21f7cb
Ninja-Tw1sT <tw1st@protonmail.com>
2019-11-30 07:52:17 -0600

    Fixed Deprecations In Server

--- Angular/server/models/db.js
+++ Angular/server/models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, (err) => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
+  useUnifiedTopology: true
     if (!err) { console.log('MongoDB connection succeeded.'); }
     else { console.log('Error in MongoDB connection : ' + JSON.stringify(err, undefined, 2)); }
 });
